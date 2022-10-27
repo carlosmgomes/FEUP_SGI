9be61982c3fb171ab8cd9db485938377968de870
Lucas <luislucas0707@gmail.com>
2022-10-27 15:13:04 +0100

    Copied TP1 base to TP2

---
+++ tp2/MyInterface.js
@@ -0,0 +1,69 @@
+import { CGFinterface, CGFapplication, dat } from '../lib/CGF.js';
+
+/**
+* MyInterface class, creating a GUI interface.
+*/
+
+export class MyInterface extends CGFinterface {
+    /**
+     * @constructor
+     */
+    constructor() {
+        super();
+    }
+
+    /**
+     * Initializes the interface.
+     * @param {CGFapplication} application
+     */
+    init(application) {
+        super.init(application);
+        // init GUI. For more information on the methods, check:
+        //  http://workshop.chromeexperiments.com/examples/gui
+
+        // add a group of controls (and open/expand by defult)
+
+        this.initKeys();
+
+        return true;
+    }
+
+    /**
+     * initKeys
+     */
+    initKeys() {
+        this.scene.gui = this;
+        this.processKeyboard = function () { };
+        this.activeKeys = {};
+    }
+
+    processKeyDown(event) {
+        this.activeKeys[event.code] = true;
+    };
+
+    processKeyUp(event) {
+        this.activeKeys[event.code] = false;
+    };
+
+    isKeyPressed(keyCode) {
+        return this.activeKeys[keyCode] || false;
+    }
+
+    /**
+     * Adds an interface containing the lights and the views.
+     */
+    startInterface(){
+        this.gui = new dat.GUI();
+        this.gui.add(this.scene, 'displayAxis').name('Display Axis');
+        this.gui.add(this.scene,'scaleFactor',0.5,3).name('Scale Factor');
+        this.lightsFolder = this.gui.addFolder("Lights");
+        for (let i = 0; i < this.scene.lights.length; i++) {
+            this.lightsFolder.add(this.scene.lights[i], 'enabled').name(this.scene.lightsIds[i]).onChange(val => { this.scene.lightVisibility(i, val);});
+        }
+        this.camerasFolder = this.gui.addFolder("Cameras");
+        this.camerasFolder.add(this.scene, 'selectedCamera', this.scene.viewsIds).name('Selected Camera').onChange(this.scene.updateCamera.bind(this.scene));
+        
+
+
+    }
+}
---
+++ tp2/MyNode.js
@@ -0,0 +1,54 @@
+/**
+ * Node class, represents a graph node.
+ */
+export class MyNode{
+   /**
+    * @constructor
+    */
+    constructor(id) {
+        this.id = id;
+        this.materials = [];
+        this.texture = [];
+        this.children_primitives=[];
+        this.children_components=[];
+        this.transfMatrix = mat4.create();
+        mat4.identity(this.transfMatrix);
+    }
+
+    /**
+     * Adds a material to the node.
+     * @param {string id} material 
+     */
+    addMaterial(material) {
+        this.materials.push(material);
+    }
+
+    /**
+     * Adds a texture to the node.
+     * @param {string id} texture 
+     * @param {number} length_s 
+     * @param {number} length_t 
+     */
+    addTexture(texture, length_s, length_t) {
+        this.texture.push(texture);
+        this.texture.push(length_s);
+        this.texture.push(length_t);
+    }
+
+
+    /**
+     * Adds a primitive to the node.
+     * @param {string id} primitive
+    */
+    addPrimitive(primitive) {
+        this.children_primitives.push(primitive);
+    }
+
+    /**
+     * Adds a component to the node.
+     * @param {string id} component
+    */
+    addComponent(component) {
+        this.children_components.push(component);
+    }
+}
