b583fce27d771e1869dbbfa1f38fbdaaddacaca0
Carlos Gomes <up201906622@edu.fe.up.pt>
2022-12-27 21:37:51 +0000

    add queen type display

--- tp3/game/MyPiece.js
+++ tp3/game/MyPiece.js
@@ -1,5 +1,5 @@
-import {CGFobject, CGFappearance} from '../../lib/CGF.js';
-import {MyPatch} from '../primitives/MyPatch.js';
+import { CGFobject, CGFappearance } from '../../lib/CGF.js';
+import { MyPatch } from '../primitives/MyPatch.js';
 
 export class MyPiece extends CGFobject {
     constructor(scene, texture, tile) {
@@ -19,13 +19,13 @@ export class MyPiece extends CGFobject {
 
         this.material.setTexture(texture);
 
-        this.controlPointsSemiSide = [[[1.8, 0, 0, 1],[1.8, 0, 0.25, 1], [1.8, 0, 0.5, 1], [1.8, 0, 1, 1]],
-                                        [[1.8, 2.4, 0, 1], [1.8, 2.4, 0.25, 1], [1.8, 2.4, 0.5, 1], [1.8, 2.4, 1, 1]],
-                                        [[-1.8, 2.4, 0, 1], [-1.8, 2.4, 0.25, 1], [-1.8, 2.4, 0.5, 1], [-1.8, 2.4, 1, 1]],
-                                        [[-1.8, 0, 0, 1], [-1.8, 0, 0.25, 1], [-1.8, 0, 0.5, 1], [-1.8, 0, 1, 1]]];
+        this.controlPointsSemiSide = [[[1.8, 0, 0, 1], [1.8, 0, 0.25, 1], [1.8, 0, 0.5, 1], [1.8, 0, 1, 1]],
+        [[1.8, 2.4, 0, 1], [1.8, 2.4, 0.25, 1], [1.8, 2.4, 0.5, 1], [1.8, 2.4, 1, 1]],
+        [[-1.8, 2.4, 0, 1], [-1.8, 2.4, 0.25, 1], [-1.8, 2.4, 0.5, 1], [-1.8, 2.4, 1, 1]],
+        [[-1.8, 0, 0, 1], [-1.8, 0, 0.25, 1], [-1.8, 0, 0.5, 1], [-1.8, 0, 1, 1]]];
 
         this.controlPointsSemiCircle = [[[0, 0, 0, 1], [0, 0, 0, 1], [0, 0, 0, 1], [0, 0, 0, 1]],
-                                        [[-1.8, 0, 0, 1], [-1.8, 0, 2.4, 1], [1.8, 0, 2.4, 1],[1.8, 0, 0, 1]]];
+        [[-1.8, 0, 0, 1], [-1.8, 0, 2.4, 1], [1.8, 0, 2.4, 1], [1.8, 0, 0, 1]]];
 
         this.semiSidePiece = new MyPatch(scene, 3, 3, 20, 20, this.controlPointsSemiSide);
         this.semiCirclePiece = new MyPatch(scene, 1, 3, 20, 20, this.controlPointsSemiCircle);
@@ -42,7 +42,7 @@ export class MyPiece extends CGFobject {
     getType() {
         return this.type;
     }
-    
+
     displayPiece() {
         this.scene.pushMatrix();
 
@@ -60,27 +60,27 @@ export class MyPiece extends CGFobject {
 
         //semi circle piece 1
         this.scene.pushMatrix();
-        this.scene.rotate(Math.PI/2, 1, 0, 0);
+        this.scene.rotate(Math.PI / 2, 1, 0, 0);
         this.scene.rotate(Math.PI, 0, 0, 1);
         this.semiCirclePiece.display();
         this.scene.popMatrix();
 
         //semi circle piece 2
         this.scene.pushMatrix();
-        this.scene.rotate(-Math.PI/2, 1, 0, 0);
+        this.scene.rotate(-Math.PI / 2, 1, 0, 0);
         this.semiCirclePiece.display();
         this.scene.popMatrix();
 
         //semi circle piece 3
         this.scene.pushMatrix();
-        this.scene.rotate(Math.PI/2, 1, 0, 0);
+        this.scene.rotate(Math.PI / 2, 1, 0, 0);
         this.scene.translate(0, 1, 0);
         this.semiCirclePiece.display();
         this.scene.popMatrix();
 
         //semi circle piece 4
         this.scene.pushMatrix();
-        this.scene.rotate(-Math.PI/2, 1, 0, 0);
+        this.scene.rotate(-Math.PI / 2, 1, 0, 0);
         this.scene.rotate(Math.PI, 0, 0, 1);
         this.scene.translate(0, 1, 0);
         this.semiCirclePiece.display();
@@ -90,11 +90,27 @@ export class MyPiece extends CGFobject {
     }
 
     display() {
-        this.scene.pushMatrix();
-        this.material.apply();
-        this.scene.scale(0.25, 0.25, 0.25);
-        this.scene.translate(2, 2, 0);
-        this.displayPiece();
-        this.scene.popMatrix();
+        if (this.type == "piece") {
+            this.scene.pushMatrix();
+            this.material.apply();
+            this.scene.scale(0.25, 0.25, 0.25);
+            this.scene.translate(2, 2, 0);
+            this.displayPiece();
+            this.scene.popMatrix();
+        }
+        else {
+            this.scene.pushMatrix();
+            this.material.apply();
+            this.scene.scale(0.25, 0.25, 0.25);
+            this.scene.translate(2, 2, 0);
+            this.displayPiece();
+            this.scene.popMatrix();
+            this.scene.pushMatrix();
+            this.material.apply();
+            this.scene.scale(0.25, 0.25, 0.25);
+            this.scene.translate(2, 2, 1.2);
+            this.displayPiece();
+            this.scene.popMatrix();
+        }
     }
 }
